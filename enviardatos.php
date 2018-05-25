2d6136f441713d012183033330103da94bebb858
juann2563 <juann2563@hotmail.com>
2018-05-25 18:35:45 -0500

    Added charts

--- enviardatos.php
+++ enviardatos.php
@@ -1,13 +1,23 @@
 <?php
 	//Obtiene los datos enviados desde el nodemcu y los guarda en la base de datos
-	$chipid = $_POST ['chipid'];
-	$temperatura = $_POST ['temperatura'];
-	try{
-		$base = new PDO('mysql:host=localhost; dbname=tutorial','root','');
-		$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO tabla (chipId, temperatura) VALUES ('".$chipid."','".$temperatura."')";	
-		$base->query($sql)
-	}catch(Exception $e){
-		
+	$chipid = $_POST ['voltaje'];
+	$temperatura = $_POST ['corriente'];
+	try {
+	$dbh = new PDO("mysql:host=localhost;dbname=tutorial",'root','');
+
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // <== add this line
+	$sql = "INSERT INTO tabla (chipId, temperatura) VALUES ('".$chipid."','".$temperatura."')";
+	if ($dbh->query($sql)) {
+	echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
 	}
+	else{
+	echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+	}
+
+	$dbh = null;
+	}
+	catch(PDOException $e)
+	{
+	echo $e->getMessage();
+	}	
 ?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<title>Laboratorio Electrónica, Telecominicaciones e informática</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/estilos.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="js/jquery.min.js"></script>
     
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
@@ -17,34 +17,48 @@
         var data = google.visualization.arrayToDataTable([
           ['Label', 'Value'],
           ['Voltaje', 0],
-          ['Corriente', 0]
-         
+          ['Corriente', 0],
+          ['Irradiación', 0],
+          ['Potencia', 0]
+         	
         ]);
+        
         var options = {
-          width: 400, height: 400,
-          redFrom: 90, redTo: 100,
-          yellowFrom:75, yellowTo: 90,
-          minorTicks: 5
+          width: 500, height: 500,
+          redFrom: 900, redTo: 1000,
+          yellowFrom:750, yellowTo: 900,
+          minorTicks: 6,
+          max: 1000
         };
         var chart = new google.visualization.Gauge(document.getElementById('Medidores'));
+        var formatter = new google.visualization.NumberFormat( {pattern: '0.00'} );
+      	formatter.format(data, 0,0);
+      	var formatter = new google.visualization.NumberFormat( {pattern: '0.00'} );
+      	formatter.format(data, 0,1);
+      	var formatter = new google.visualization.NumberFormat( {pattern: '0.00'} );
+      	formatter.format(data, 0,2);
+      	var formatter = new google.visualization.NumberFormat( {pattern: '0.00'} );
+      	formatter.format(data, 0,3);
         chart.draw(data, options);
         setInterval(function() {
             var JSON=$.ajax({
-                url:"http://localhost/ETI/DatoSensores.php?q=1",
+                url:"http://localhost/ETI/datoSensores.php?q=1",
                 dataType: 'json',
                 async: false}).responseText;
             var Respuesta=jQuery.parseJSON(JSON);
             
-          data.setValue(0, 1,Respuesta[0].chipid);
-          data.setValue(1, 1,Respuesta[0].temperatura);
-          chart.draw(data, options);
-        }, 1300);
+          data.setValue(0, 1,(Respuesta[0].temperatura));
+          data.setValue(1, 1,(Respuesta[0].chipid));
+          data.setValue(2, 1,(Respuesta[0].temperatura*(1000.0/22.1)));
+          data.setValue(3, 1,(Respuesta[0].temperatura*Respuesta[0].chipid));
+        chart.draw(data, options);
+        }, 1000);
         
       }
     </script>
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-static-top">
+	<nav class="navbar navbar-default navbar-static-top" style="background: #7B1FA2;">
 		<div class="container">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -53,26 +67,26 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Zabala</a>
+				<a class="navbar-brand" href="#" style="color: white;">ETI</a>
 			</div>
 			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav">
-					<li>
+					<!-- <li>
 						<a href="#">Entradas</a>
-					</li>
-					<li>
+					</li> -->
+<!-- 					<li>
 						<a href="#">Favoritos</a>
 					</li>
 					<li>
 						<a href="#">Autores</a>
-					</li>
+					</li> -->
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
 					<li>
-						<a href="#">Iniciar sesión</a>
+						<a href="#" style="color: white;">Iniciar sesión</a>
 					</li>
 					<li>
-						<a href="#">Registro</a>
+						<a href="#" style="color: white;">Registro</a>
 					</li>					
 				</ul>
 			</div>
@@ -88,12 +102,12 @@
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4">
+			<!-- <div class="col-md-4">
 				<div class="row">
 					<div class="col md-12">
 						<div class="panel panel-default">
-							<div class="panel-heading">
-								<span class="glyphicon glyphicon-search" aria-hidden="true"></span> Búsqueda
+							<div class="panel-heading" style="background: #9C27B0;">
+								<span class="glyphicon glyphicon-search" aria-hidden="true" style="color: white;"></span> Búsqueda
 							</div>
 							<div class="panel-body">
 								<div class="form-group">
@@ -106,16 +120,16 @@
 						</div>
 					</div>
 				</div>				
-			</div>
-			<div class="col-md-8">
+			</div> -->
+			<div class="col-md-12">
 				<div class="panel panel-default">
-					<div class="panel-heading">
-						<span class="glyphicon glyphicon-time" aria-hidden="true"></span> Últimas entradas
+					<div class="panel-heading" style="background: #9C27B0;">
+						<span aria-hidden="true" style="color: white;">Medidores</span>
 					</div>
 					<div class="panel-body">
 						<div id="Medidores"></div>
 						
-						<p>Todavía no hay entradas</p>
+						<!-- <p>Todavía no hay entradas</p> -->
 					</div>
 				</div>
 			</div>
